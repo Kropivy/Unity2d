c282787fb7ffb4da7d59e9da2d53737f78b8f2d2
Kropivy <64264649+Kropivy@users.noreply.github.com>
2020-05-19 17:34:45 +0300

    Unity 2d
    Unity prjgeky unity githab

---
+++ Assets/Scenes/skripts.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skripts : MonoBehaviour
+{
+    int a;
+ 
+    void Start()
+    {
+        Debug.Log("Привет");
+    }
+
+  
+    void Update()
+    {
+        
+    }
+}
